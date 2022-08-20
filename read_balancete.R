b2dcfc483650a892b3e055917d9fe7357fde6200
andreferraribr <andre.ferrari.br@gmail.com>
2022-08-20 17:47:54 -0300

    nomes colunas

--- read_balancete.R
+++ read_balancete.R
@@ -22,11 +22,19 @@ writeLines(balancete_clean, "balancete_clean.txt")
 balancete <- as.data.frame(  read.fortran("balancete_clean.txt",c("A15","A40","A21","A1")))
 write_csv2(balancete,"balancete.csv")
 
-balancete <- balancete %>% mutate(valor = parse_number(V3, locale = locale(decimal_mark = ",", grouping_mark = ".")))
+
+
+names(balancete) <- c("conta_id_siafi","conta_siafi","saldo_siafi","tipo_saldo")
+
+balancete <- balancete %>% mutate(saldo_siafi = parse_number(saldo_siafi, locale = locale(decimal_mark = ",", grouping_mark = ".")))
+
+balancete <- balancete %>% mutate(conta_id = ( str_replace_all(balancete$conta_id_siafi, "[^[:alnum:]]", "")))
+
+balancete$conta_id <- type.convert(balancete$conta_id)
 
 datatable(balancete) %>% formatCurrency(
   
-  "valor",
+  "saldo_siafi",
   currency = "R$ ",
   interval = 3,
   mark = ".",
@@ -37,74 +45,3 @@ datatable(balancete) %>% formatCurrency(
   rows = NULL
 )
 
-tabela_reais = function (df,coluna = NULL) {
-  datatable((df)%>%
-              # "row" para o total aparecer na linha, ou seja, totalizar os valores de uma coluna
-              adorn_totals("row") ,
-            filter = 'top', 
-            rownames = FALSE,
-            extensions = 'Buttons',
-            options = list( 
-              # order = list (df[(length(df))], 'desc'),
-              dom = "Blfrtip",
-              buttons = 
-                list("copy", list(
-                  extend = "collection",
-                  buttons = c("csv", "excel", "pdf"),
-                  text = "Download" ) ),
-              lengthMenu = list( c(-1, 5, 10,20),
-                                 c( "tudo",5, 10, 20)),
-              pageLength = -1 )
-  )%>%
-    formatRound(
-      # formatar apenas as colunas numericas.
-      # sapply para identificar as colunas numericas e combinar com o parametro COLUNA
-      # ((ncol(df %>% select_if(is.character))+1):(ncol(df )+1)),
-      # http://datamining.togaware.com/survivor/Remove_Non_Numeric.html
-      (c(colnames(df[,sapply(df, is.numeric)]), coluna)),
-      digits = 2,
-      interval = 3,
-      mark = ".",
-      dec.mark = ","
-    ) 
-}
-
-
-
-# library(readr)
-# balancete_2 <- read_delim("balancete.csv", delim = ";", 
-#                   escape_double = FALSE, locale = locale(decimal_mark = ",", 
-#                                                          grouping_mark = "."), trim_ws = TRUE)
-
-
-# sink(file = "texto.txt")
-# (tt21)
-# sink(file = NULL)
-# 
-# aaa <- as.data.frame(tt21)
-# balancete_siafi <- as.data.frame(  read.fortran("texto.txt",c("A8","A15","A40","A21","A1")))
-# 
-# balancete_siafi <- as.data.frame(  read.fortran("texto.txt",c("A8","A15","A40","A21","A1")))
-# 
-# write.csv2(balancete_siafi,"balancete.cvs")
-# 
-# balancete <- read_delim("balancete.cvs", 
-#                         delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ",", 
-#                                                                             grouping_mark = ".", encoding = "latin1"), 
-#                         trim_ws = TRUE)
-# 
-# balancete <- read_delim("balancete_clean.txt", 
-#                         delim = ";", escape_double = FALSE, locale = locale(decimal_mark = ",", 
-#                                                                             grouping_mark = ".", encoding = "latin1"), 
-#                         trim_ws = TRUE)
-# 
-# 
-# library(dplyr)
-# library(tidyr)
-# 
-# gg <- tt21 %>%
-#   data.frame(tt21 = .) %>%
-#   extract(tt21, c("x","y","z","w"), "^(\\a{15})(\\a{34})(\\a{21})(\\a{1})", remove = FALSE)
-# 
-# 
-# 
